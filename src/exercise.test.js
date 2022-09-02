d1176291178e01257cd2b5623b228f167f74e8c3
Nicolas Molina Monroy <hi@nicobytes.com>
2022-09-02 16:27:06 -0400

    :construction: work

--- src/exercise.test.js
+++ src/exercise.test.js
@@ -1,7 +1,8 @@
 import { runCode } from "./exercise";
 
+
 describe("tests", () => {
   it("should return Throw a error", async () => {
-    expect(async () => await runCode()).toThrow(new Error("API No Found"));
+    expect(() => runCode()).rejects.toThrowError(new Error("API Not Found"));
   });
 });
